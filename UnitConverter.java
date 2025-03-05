9fed0461ada99b226618655af0de60019973c1e5
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-05 22:16:28 +0530

    Adding more unit converting functions to the program

--- UnitConverter.java
+++ UnitConverter.java
@@ -19,10 +19,40 @@ public class UnitConverter {
         return feet * feet2meters;
     }
 
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    public static double convertInchesToCm(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
     public static void main(String[] args) {
         System.out.println("10 km in miles: " + convertKmToMiles(10));
         System.out.println("10 miles in km: " + convertMilesToKm(10));
         System.out.println("10 meters in feet: " + convertMetersToFeet(10));
         System.out.println("10 feet in meters: " + convertFeetToMeters(10));
+        System.out.println("10 yards in feet: " + convertYardsToFeet(10));
+        System.out.println("10 feet in yards: " + convertFeetToYards(10));
+        System.out.println("10 meters in inches: " + convertMetersToInches(10));
+        System.out.println("10 inches in meters: " + convertInchesToMeters(10));
+        System.out.println("10 inches in cm: " + convertInchesToCm(10));
     }
 }
