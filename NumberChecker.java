a540db9fc3d731b755413206a82e14a2f16ee725
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-05 22:25:34 +0530

    Program to check the number is positive, negative, zero

---
+++ NumberChecker.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class NumberChecker {
+
+    public static boolean isPositive(int number) {
+        return number >= 0;
+    }
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static int compare(int number1, int number2) {
+        if (number1 > number2) {
+            return 1;
+        } else if (number1 < number2) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPositive(numbers[i])) {
+                System.out.print("Number " + numbers[i] + " is positive and ");
+                if (isEven(numbers[i])) {
+                    System.out.println("even.");
+                } else {
+                    System.out.println("odd.");
+                }
+            } else {
+                System.out.println("Number " + numbers[i] + " is negative.");
+            }
+        }
+
+        int comparison = compare(numbers[0], numbers[numbers.length - 1]);
+        if (comparison == 1) {
+            System.out.println("First element is greater than last element.");
+        } else if (comparison == -1) {
+            System.out.println("First element is less than last element.");
+        } else {
+            System.out.println("First and last elements are equal.");
+        }
+
+        input.close();
+    }
+}
