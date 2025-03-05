ad84fbceafd2625b04bd039c8dde8ff01e5d06c0
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-05 22:21:21 +0530

    Program to check is the student can or not

---
+++ StudentVote.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class StudentVote {
+    
+    public static boolean canStudentVote(int age) {
+        if (age < 0) {
+            return false;
+        }
+        return age >= 18;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] ages = new int[10];
+        
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = input.nextInt();
+        }
+        
+        for (int i = 0; i < ages.length; i++) {
+            if (canStudentVote(ages[i])) {
+                System.out.println("Student " + (i + 1) + " is eligible to vote.");
+            } else {
+                System.out.println("Student " + (i + 1) + " is not eligible to vote.");
+            }
+        }
+        
+        input.close();
+    }
+}
