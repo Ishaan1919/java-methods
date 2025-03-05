7455eaf66d2eeeec09f00fe833a7a9de18bbe2d0
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-05 09:32:17 +0530

    Program to find the number of handshakes

---
+++ HandShakes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HandShakes {
+    public static int calculateHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int n = input.nextInt();
+
+        int maxHandshakes = calculateHandshakes(n);
+
+        System.out.println("The maximum number of handshakes among " + n + " students is " + maxHandshakes);
+
+        input.close();
+    }
+}
